4c41f2cf5ffef6ddb5025ad47aa292994835f789
Nate Bosley <natebosley@Nate-Bosleys-MacBook-Pro.local>
2018-12-01 15:51:05 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
 console.log("Up and running!");
 
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne;
+cards[0] = cardOne;
+cards.push("queen");
+console.log("User flipped Queen");
+
+var cardTwo;
+cards[2] = cardTwo;
+cards.push("king");
+console.log("User flipped King");
+
+if (cards.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+}
+
+else if (cards.length !== 2); {
+alert("Sorry Try Again!");
+}
+
